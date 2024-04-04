820374e7a52ffae3893c9e4298d63e78b3542979
AeddAenye <flavymettis@gmail.com>
2024-04-04 19:33:30 +0700

    переход на виту

--- frontend/vue/vite.config.js
+++ frontend/vue/vite.config.js
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "Anona",
+  plugins: [
+    vue(),
+  ],
+  base: 'Anona',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
