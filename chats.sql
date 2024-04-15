4f86155904b1e6f365e38eaf702158d79a763eb0
AeddAenye <flavymettis@gmail.com>
2024-04-15 07:43:54 +0700

    Загрузил sql

---
+++ chats.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: mysql:3306
+-- Время создания: Апр 15 2024 г., 00:43
+-- Версия сервера: 8.3.0
+-- Версия PHP: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `anona`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `chats`
+--
+-- Структура чтения ошибок для таблицы anona.chats: #1049 - Неизвестная база данных &#039;anona&#039;
+-- Ошибка считывания данных таблицы anona.chats: #1064 - У вас ошибка в запросе. Изучите документацию по используемой версии MySQL на предмет корректного синтаксиса около &#039;FROM `anona`.`chats`&#039; на строке 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
